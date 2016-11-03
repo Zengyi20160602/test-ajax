c709a4177ea076e5ae4e14a0cfd4935f9db80459
Zengyi20160602 <3054667507@qq.com>
2016-11-03 17:03:09 +0800

    Update dealData.js

--- dealData.js
+++ dealData.js
@@ -39,14 +39,12 @@
 			var nameNode,numNode,telNode,displayText;
 			var rootNode,firstNode;
 			rootNode = xmldoc.documentElement;  //获取根节点
-			firstNode = rootNode.firstChild;    //访问根节点下的第一个节点
-			nameNode = firstNode.firstChild;    //获取name元素
-			numNode = nameNode.nextSibling;     //获取name元素的兄弟元素->num元素
-			telNode = numNode.nextSibling;      //获取tel元素
-			//实现字符串的拼接，输出XML文档中的数据
-			displayText = "姓名:" + nameNode.firstChild.nodeValue + "<br>" +
-						"编号:" + numNode.firstChild.nodeValue + "<br>" +
-						"电话:" + telNode.firstChild.nodeValue;
+			firstNode = rootNode.children(0);    //访问根节点下的第一个节点
+			for(var i=0,i<rootNode.children.length,i++)
+			{
+				displayText = displayText + 
+					firstNode.children(i).nodeValue + \n;
+			}
 			return displayText;
 		}
 		
